5c6aee1ef71460ef3fb5fa2294d510dddca9dcd4
unknown <daniel.fogaca.75@hotmail.com>
2023-01-10 13:48:38 -0300

    feat: create navigation routes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome</Text>
-    </View>
-  );
-};
+import Routes from './routes';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-});
-
-export default App;
+export default function App() {
+  return <Routes />;
+}
--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect} from 'react';
+import {View, Image, TouchableOpacity} from 'react-native';
 
-// import { Container } from './styles';
+import camera from '../assets/camera.png';
 
 const Feed = () => {
   return <View />;
+};
+
+Feed.navigationOptions = ({navigation}) => {
+  return {
+    headerRight: (
+      <TouchableOpacity
+        style={{marginRight: 20}}
+        onPress={() => {
+          navigation.navigate('New');
+        }}>
+        <Image source={camera} />
+      </TouchableOpacity>
+    ),
+  };
 };
 
 export default Feed;
