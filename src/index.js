0d3c798697f83b71d9df2100f8fd24648693a186
unknown <daniel.fogaca.75@hotmail.com>
2023-01-10 11:15:39 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+});
+
+export default App;
